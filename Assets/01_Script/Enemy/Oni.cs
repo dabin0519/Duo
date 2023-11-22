442ddb31a3328638899e913c547b0d06bf58fd52
dabin0519 <dadabin0519@naver.com>
2023-11-23 00:50:39 +0900

    oni ing

--- Assets/01_Script/Enemy/Oni.cs
+++ Assets/01_Script/Enemy/Oni.cs
@@ -1,28 +1,43 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Oni : MonoBehaviour
 {
+    [SerializeField] private float _protectTime;
+    [SerializeField] private float _protectPosY;
+    [SerializeField] private GameObject _protect;
+
     private Animator _oniAnim;
-    private CapsuleCollider2D _capsuleCollider;
+    private CircleCollider2D _circleCollider;
     private ScoreSystem _scoreSystem;
     private CityHealth _cityHealth;
+    private Movement _movement;
 
     private int _hp = 3;
 
     private void Awake()
     {
         _oniAnim = GetComponent<Animator>();
-        _capsuleCollider = GetComponent<CapsuleCollider2D>();
+        _circleCollider = GetComponent<CircleCollider2D>();
+        _movement = GetComponent<Movement>();
         _scoreSystem = FindObjectOfType<ScoreSystem>();
         _cityHealth = FindObjectOfType<CityHealth>();
     }
 
     private void Update()
     {
-        
+        if (gameObject.transform.position.y <= -7)
+        {
+            Destroy(gameObject);
+            _cityHealth.OnDamage();
+            _scoreSystem.Score -= 100;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OnHurt();
+        }
     }
 
     public void OnHurt()
@@ -31,11 +46,41 @@ public class Oni : MonoBehaviour
 
         if(_hp > 0)
         {
-
+            _oniAnim.SetBool("IsProtect", true);
+            StartCoroutine(Protect());
         }
         else
         {
             _oniAnim.SetTrigger("Die");
+            Destroy(_circleCollider);
         }
+    }
+
+    private IEnumerator Protect()
+    {
+        // 뒤로 밀리기
+        _movement.enabled = false;
+        Vector3 curPos =  transform.position;
+        float time = 0;
+        while(time < _protectTime)
+        {
+            transform.position = new Vector3(curPos.x, curPos.y + EaseOutQuad(time / _protectTime) * _protectPosY);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+        _movement.enabled = true;
+        _oniAnim.SetBool("IsProtect", false);
+    }
+
+    private float EaseOutQuad(float x)
+    {
+        return 1 - (1 - x) * (1 - x);
+    }
+
+    public void FinishDie()
+    {
+        Destroy(gameObject);
+        _scoreSystem.Score += 150;
     }
 }
